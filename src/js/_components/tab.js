2b4735d6262b09a5318bab86726f08a04065c295
yuji nakagawa <nakagawa@nulab.co.jp>
2017-08-06 12:43:01 +0900

    rename var

--- src/js/_components/tab.js
+++ src/js/_components/tab.js
@@ -3,8 +3,8 @@ import Cookie from "./cookie.js";
 (function () {
     var _tabs = document.querySelectorAll("[data-js-tab]");
     for (var h = 1; h <= _tabs.length; h++) {
-        var tab_element_number = "[data-js-tab='" + h + "']";
-        CookieTab(tab_element_number, h);
+        var _tab_element_number = "[data-js-tab='" + h + "']";
+        CookieTab(_tab_element_number, h);
     }
 }());
 
@@ -13,20 +13,20 @@ function CookieTab(tabElm, tabNum) {
     var _tab_nav_list = _tab_element.querySelectorAll("[data-js-tab-nav]");
     var _tab_content_list = _tab_element.querySelectorAll("[data-js-tab-content]");
 
-    var _savedActiveTabNumber = Cookie.get("myComponentTabNumber_" + tabNum); //get cookie and active the tab
-    tabClick(_tab_element.querySelector("[data-js-tab-nav='" + _savedActiveTabNumber + "']"));
+    var _saved_active_tab_number = Cookie.get("myComponentTabNumber_" + tabNum); //get cookie and active the tab
+    tabClick(_tab_element.querySelector("[data-js-tab-nav='" + _saved_active_tab_number + "']"));
 
     function tabClick(elm) {
-        var _thisTab;
+        var _this_tab;
         if( elm instanceof HTMLElement ) {
-            _thisTab = elm;
+            _this_tab = elm;
         } else if( elm === null ) {
             return;
         } else {
-            _thisTab = elm.target;
+            _this_tab = elm.target;
         }
-        var _thisTabNumber = _thisTab.getAttribute("data-js-tab-nav");
-        var _thisTabContent = _tab_element.querySelector("[data-js-tab-content='" + _thisTabNumber + "']");
+        var _this_tab_number = _this_tab.getAttribute("data-js-tab-nav");
+        var _this_tab_content = _tab_element.querySelector("[data-js-tab-content='" + _this_tab_number + "']");
 
         // if the tab has .is_active, remove roop
         for (var i = 0; i < _tab_nav_list.length; i++) {
@@ -41,9 +41,9 @@ function CookieTab(tabElm, tabNum) {
             }
         }
 
-        _thisTab.classList.add("is_active");
-        _thisTabContent.classList.add("is_active");
-        return Cookie.set("myComponentTabNumber_" + tabNum, _thisTabNumber, 90, "localhost", "/"); // cookie name and tab num, keep dates
+        _this_tab.classList.add("is_active");
+        _this_tab_content.classList.add("is_active");
+        return Cookie.set("myComponentTabNumber_" + tabNum, _this_tab_number, 90, "localhost", "/"); // cookie name and tab num, keep dates
     }
 
     // Each tabs addEventListener
